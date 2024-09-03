e62c0556830fd6c53f13c9c889d9a27a7f287e4e
AlexDyakonov <shram.monolit@mail.ru>
2024-09-03 20:53:44 +0300

    Updated tagService

--- src/services/tagService.tsx
+++ src/services/tagService.tsx
@@ -4,29 +4,24 @@ import { API_BASE_URL } from '../config';
 export interface Tag {
     id?: number;
     name: string;
-    url: string;
+    icon: string;
 }
 
 export const getTags = async (): Promise<Tag[]> => {
-    const response = await axios.get(`${API_BASE_URL}/tags`);
-    return response.data;
-};
-
-export const getTagById = async (id: number): Promise<Tag> => {
-    const response = await axios.get(`${API_BASE_URL}/tags/${id}`);
+    const response = await axios.get(`${API_BASE_URL}/places/tags`);
     return response.data;
 };
 
 export const createTag = async (tag: Tag): Promise<Tag> => {
-    const response = await axios.post(`${API_BASE_URL}/tags`, tag);
+    const response = await axios.post(`${API_BASE_URL}/places/tags`, tag);
     return response.data;
 };
 
 export const updateTag = async (id: number, tag: Tag): Promise<Tag> => {
-    const response = await axios.put(`${API_BASE_URL}/tags/${id}`, tag);
+    const response = await axios.put(`${API_BASE_URL}/places/tag/${id}`, tag);
     return response.data;
 };
 
 export const deleteTag = async (id: number): Promise<void> => {
-    await axios.delete(`${API_BASE_URL}/tags/${id}`);
+    await axios.delete(`${API_BASE_URL}/places/tag/${id}`);
 };
