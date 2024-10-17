2ed9f0c5250dc793a181275aa02582630972a31a
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-10-17 20:06:47 +0300

    lint: fix

--- src/commands/join.ts
+++ src/commands/join.ts
@@ -1,9 +1,9 @@
-import { BOT_USERNAME } from 'src/config';
-import { createLobby } from 'src/services/lobbyService';
-import { Context, Telegraf } from 'telegraf';
+import { BOT_USERNAME } from "src/config";
+import { createLobby } from "src/services/lobbyService";
+import { Context, Telegraf } from "telegraf";
 
 export function setupJoinCommand(bot: Telegraf<Context>) {
-  bot.command('join', async (ctx) => {
+  bot.command("join", async (ctx) => {
     const getLobbyUrl = async () => {
       const lobby = await createLobby({
         location: { lat: 59.9311, lon: 30.3609 },
@@ -16,10 +16,10 @@ export function setupJoinCommand(bot: Telegraf<Context>) {
 
     if (lobby) {
       await ctx.reply(
-        `Лобби успешно создано! Присоединяйтесь по ссылке: [Lobby #${lobby.id}](https://t.me/${BOT_USERNAME}/app?startapp=${lobby.id})`
+        `Лобби успешно создано! Присоединяйтесь по ссылке: [Lobby #${lobby.id}](https://t.me/${BOT_USERNAME}/app?startapp=${lobby.id})`,
       );
     } else {
-      await ctx.reply('К сожалению, не удалось создать лобби.');
+      await ctx.reply("К сожалению, не удалось создать лобби.");
     }
   });
 }
