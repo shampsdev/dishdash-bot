9e76d0a6c668d6a403eb14c5aa5f54068b1de9af
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-11-14 21:12:24 +0300

    fix(lint)

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -2,23 +2,22 @@ import { BOT_USERNAME } from "src/config";
 import { Context, Telegraf } from "telegraf";
 
 export function setupStartCommand(bot: Telegraf<Context>) {
-    bot.start(async (ctx) => {
-        const username = ctx.from.username || "unknown";
-        await ctx.reply(
-            `Добро пожаловать, @${username}!\nЧтобы воспользоваться ботом, откройте мини-приложение DishDash:`,
-            {
-                reply_markup: {
-                    inline_keyboard: [
-                        [
-                            {
-                                text: "Открыть DishDash 🤌",
-                                url: `https://t.me/${ctx.botInfo.username}/app`,
-                            },
-                        ],
-                    ],
-                },
-            },
-        );
-    });
-
+  bot.start(async (ctx) => {
+    const username = ctx.from.username || "unknown";
+    await ctx.reply(
+      `Добро пожаловать, @${username}!\nЧтобы воспользоваться ботом, откройте мини-приложение DishDash:`,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: "Открыть DishDash 🤌",
+                url: `https://t.me/${ctx.botInfo.username}/app`,
+              },
+            ],
+          ],
+        },
+      },
+    );
+  });
 }
