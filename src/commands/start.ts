88ad5c12a1af3182ac473b7a78fffed8a1cd55f7
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-11-25 13:49:44 +0300

    updated start command

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -4,7 +4,7 @@ import { Context, Telegraf } from "telegraf";
 export function setupStartCommand(bot: Telegraf<Context>) {
     bot.start(async (ctx) => {
         await ctx.replyWithPhoto(
-            "https://storage.yandexcloud.net/dishash-s3/assets/bot/cover-new.png",
+            "https://storage.yandexcloud.net/dishash-s3/assets/bot/cover.jpeg",
             {
                 caption: `*Как свайпать?*\n\n• Чтобы посмотреть места в одиночку, запустите бота по кнопке ниже\n\n• Чтобы искать места вместе, запустите бота тут и отправьте приглашение в лобби, или тегните @dishdash\\_bot в любом чате`,
                 parse_mode: "MarkdownV2",
@@ -20,20 +20,5 @@ export function setupStartCommand(bot: Telegraf<Context>) {
                 },
             },
         );
-
-        await ctx.replyWithMediaGroup([
-            {
-                type: "photo",
-                media:
-                    "https://storage.yandexcloud.net/dishash-s3/assets/bot/card_1.jpeg",
-                caption: `*Как свайпать?*\n\n• Чтобы посмотреть места в одиночку, запустите бота по кнопке ниже\n\n• Чтобы искать места вместе, запустите бота тут и отправьте приглашение в лобби, или тегните @dishdash\\_bot в любом чате`,
-                parse_mode: "MarkdownV2",
-            },
-            {
-                type: "photo",
-                media:
-                    "https://storage.yandexcloud.net/dishash-s3/assets/bot/card_2.jpeg",
-            },
-        ]);
     });
 }
