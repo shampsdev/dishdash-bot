fbe7927f39a79f568bb2edea35af511f581cadb5
timur <priest_faria@timurs-MacBook-Air.local>
2024-12-14 16:35:02 +0300

    refactor(/stop_feedback command): removed

--- src/commands/feedback.ts
+++ src/commands/feedback.ts
@@ -1,53 +1,21 @@
 import { MyContext } from "src/bot";
 import { IFeedbackService } from "src/services/feedbackService";
 import logger from "src/utils/logger";
-import { Context, Telegraf } from "telegraf";
+import { Telegraf } from "telegraf";
 import { FEEDBACK_CHAT_ID } from "src/config";
-import { CommandContextExtn } from "node_modules/telegraf/typings/telegram-types";
-import { Update, Message } from "telegraf/types";
 // TODO: voice message transcription
 export function setupFeedbackCommand(
   bot: Telegraf<MyContext>,
-  fbService: IFeedbackService,
+  fbService: IFeedbackService
 ) {
-  const stopFeedback = (
-    ctx: Context<{
-      message: Update.New & Update.NonChannel & Message.TextMessage;
-      update_id: number;
-    }> &
-      Omit<MyContext, keyof Context<Update>> &
-      CommandContextExtn,
-  ) => {
-    if (ctx.session !== undefined) {
-      if (ctx.session.feedback_mode) {
-        logger.info(
-          `Feedback mode set to false for user ${ctx.from.username ?? ctx.from.first_name}`,
-        );
-        ctx.reply("Ты перестал делиться фидбэком");
-        ctx.session.feedback_mode = false;
-
-        fbService.sendFeedbackEnd({
-          username: ctx.message.from.username
-            ? `@${ctx.message.from.username}`
-            : ctx.message.from.id.toString(),
-        });
-        setTimeout(() => {
-          stopFeedback(ctx);
-        }, 1000 * 90);
-      }
-    }
-  };
-
   bot.command("feedback", async (ctx) => {
     if (ctx.session?.feedback_mode) {
       return;
     }
     logger.info(
-      `Feedback mode set to true for user ${ctx.from.username ?? ctx.from.first_name}`,
+      `Feedback mode set to true for user ${ctx.from.username ?? ctx.from.first_name}`
     );
-    ctx.reply(
-      "Пришли свой фидбэк в любом формате!\nДля того, чтобы перестать делиться, напиши команду /stop_feedback",
-    );
+    ctx.reply("Пришли свой фидбэк в любом формате!\n");
     ctx.session = {
       feedback_mode: true,
     };
@@ -58,10 +26,6 @@ export function setupFeedbackCommand(
     });
   });
 
-  bot.command("stop_feedback", async (ctx) => {
-    stopFeedback(ctx);
-  });
-
   bot.on("message", async (ctx, next) => {
     if (ctx.session === undefined || ctx.session.feedback_mode !== true)
       return next();
@@ -70,9 +34,12 @@ export function setupFeedbackCommand(
     if (targetChatId === undefined) {
       return next();
     }
+    if (ctx.session.feedback_mode) {
+      await ctx.forwardMessage(targetChatId);
+    }
 
-    await ctx.forwardMessage(targetChatId);
-
+    ctx.reply("Спасибо за фидбек!");
+    ctx.session.feedback_mode = false;
     return next();
   });
 }
