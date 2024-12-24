c142b6d3e5bf44978a08b9b40a54a1a35eeeb59f
Mike de Geofroy <mikedegeofroy@gmail.com>
2024-12-24 10:27:04 +0300

    feat(feedback) (#17)
    * Added await for feedback message
    
    * Added metrics to start bot
    
    * added user to mertics
    
    * fix(lint)
    
    * disabled /help command
    
    * fixed formatting
    
    * edit start and feedback messages inner
    
    * add: handling feedback redirection from miniapp
    
    ---------
    
    Co-authored-by: timur <priest_faria@timurs-MacBook-Air.local>
    Co-authored-by: Timur Valeev <76071256+PriestFaria@users.noreply.github.com>

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,31 +1,54 @@
+import { MyContext } from "src/bot";
 import { BOT_USERNAME } from "src/config";
 import { IMetricService } from "src/services/metricService";
-import { Context, Telegraf } from "telegraf";
+import logger from "src/utils/logger";
+import { Telegraf } from "telegraf";
 
 export function setupStartCommand(
-  bot: Telegraf<Context>,
+  bot: Telegraf<MyContext>,
   metricService: IMetricService,
 ) {
   bot.start(async (ctx) => {
     const userId = ctx.from.id;
     metricService.sendTagEvent(userId, ctx.payload);
 
-    await ctx.replyWithPhoto(
-      "https://storage.yandexcloud.net/dishash-s3/assets/bot/cover.jpeg",
-      {
-        caption: `*Как найти идеальное место для встречи?*\n\n1\\. Нажмите кнопку «Open»\n2\\. Создайте лобби\n3\\. Отправьте приглашения, если будете выбирать вместе\n4\\. Настройте параметры поиска под ваши цели и бюджет\n5\\. Все готово – начинайте свайпать\\!`,
-        parse_mode: "MarkdownV2",
-        reply_markup: {
-          inline_keyboard: [
-            [
-              {
-                text: "Открыть DishDash 🤌",
-                url: `https://t.me/${BOT_USERNAME}/app`,
-              },
-            ],
-          ],
+    const paramsPayload = ctx.payload;
+
+    if (paramsPayload === "feedback") {
+      if (ctx.session?.feedback_mode) {
+        return;
+      }
+      logger.info(
+        `Feedback mode set to true for user ${ctx.from.username ?? ctx.from.first_name}`,
+      );
+      await ctx.replyWithPhoto(
+        "https://storage.yandexcloud.net/dishash-s3/assets/bot/feedback.png",
+        {
+          caption: `Поделитесь своим мнением о DishDash в любом удобном формате\\. Отправьте фото\\, видео или текст прямо здесь\\, а бот передаст отзыв команде разработки\\!`,
+          parse_mode: "MarkdownV2",
         },
-      },
-    );
+      );
+      ctx.session = {
+        feedback_mode: true,
+      };
+    } else {
+      await ctx.replyWithPhoto(
+        "https://storage.yandexcloud.net/dishash-s3/assets/bot/cover.jpeg",
+        {
+          caption: `*Как найти идеальное место для встречи?*\n\n1\\. Нажмите кнопку «Open»\n2\\. Создайте лобби\n3\\. Отправьте приглашения, если будете выбирать вместе\n4\\. Настройте параметры поиска под ваши цели и бюджет\n5\\. Все готово – начинайте свайпать\\!`,
+          parse_mode: "MarkdownV2",
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "Открыть DishDash 🤌",
+                  url: `https://t.me/${BOT_USERNAME}/app`,
+                },
+              ],
+            ],
+          },
+        },
+      );
+    }
   });
 }
